e818211ea228f3d51a2237b4377e81425b33ed3a
Vadim <vadim@hyperweb.com.ua>
2018-01-10 15:07:08 +0200

    adding methods to getting from IndexQueries class

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,9 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\IndexQueries;
+
 class IndexController extends Controller
 {
-    public function index(){
-      return view('index');
-    }
+  private $query;
+
+  public function index(){
+
+   $query = new IndexQueries();
+     //get top hosters
+   $top_hosters_summary =  $query->getTopHosters();
+   //gte top hosters by region
+  $top_hosters_by_region_ru = $query->getTopHostersByRegion('RU');
+  $top_hosters_by_region_uk = $query->getTopHostersByRegion('UK');
+
+   return view('index',compact('top_hosters_summary', 'top_hosters_by_region_ru', 'top_hosters_by_region_uk'));
+ }
 }
