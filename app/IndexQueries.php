3c88408d98476a300691625c386aa411d7933da5
Vadim <vadim@hyperweb.com.ua>
2018-01-11 16:45:53 +0200

    wrote methods to getting top of hosters by kind (vps, vds, vps server)

--- app/IndexQueries.php
+++ app/IndexQueries.php
@@ -1,35 +1,27 @@
 <?php
 namespace App;
 use Illuminate\Support\Facades\DB;
-use App\Hoster;
+use App\Models\Hoster;
+use App\AbstractClasses\AbstractIndexQueries;
 /**
  * Here is main logic for working with DB
  *
  * Class make some methods for working with database and models
 */
-class IndexQueries {
-/**
- * list of hosters
- * @var Object
- */
-public $hosters;
-/**
- * getting top hosters on index page
- * @var $limit INT limit of count result
- *     
- * @return JSON object with name and ID max 10 hosters
- */
-public function getTopHosters($limit=10){
-  $hosters = DB::table('hosters')
+class IndexQueries extends AbstractIndexQueries{
+
+protected function topHosters($limit){
+  $this->hosters = DB::table('hosters')
   ->join('raitings','hosters.hoster_id','=','raitings.hoster_id')
   ->select('name','hosters.hoster_id')
   ->orderBy('rait')
   ->limit($limit)
   ->get();
-  return $hosters;
+  return $this->hosters;
 }
-public function getTopHostersByRegion($region='UK',$limit=10){
-  $hosters = DB::table('hosters')
+
+protected function topHostersByRegion($region,$limit){
+  $this->hosters = DB::table('hosters')
   ->join('raitings','hosters.hoster_id','=','raitings.hoster_id')
   ->join('regions','regions.id','=','hosters.region')
   ->select('name','hosters.hoster_id')
@@ -37,6 +29,32 @@ public function getTopHostersByRegion($region='UK',$limit=10){
   ->orderBy('rait')
   ->limit($limit)
   ->get();
-  return $hosters;
+  return $this->hosters;
+}
+
+protected function topHostersByKind($kind,$limit){
+  $this->hosters = DB::table('types_of_hostings')
+  ->select('hosters.name', 'hosters.hoster_id')
+  ->join('hosters','types_of_hostings.hoster_id','=','hosters.hoster_id')
+  ->join('kind_of_hostings','types_of_hostings.kind_of_hosting_id','=','kind_of_hostings.kind_id')
+  ->join('raitings','raitings.hoster_id','=','hosters.hoster_id')
+  ->where('kind_of_hostings.caption','=',$kind)
+  ->orderBy('rait')
+  ->limit($limit)
+  ->get();
+  return $this->hosters;
+}
+public function getTopHosters($limit=10){
+  $this->hosters= $this->topHosters($limit);
+  return $this->hosters;
+}
+
+public function getTopHostersByRegion($region="UK",$limit=10){
+  $this->hosters = $this->topHostersByRegion($region,$limit);
+  return $this->hosters;
+}
+public function getTopHostersByKind($kind='vps',$limit=5){
+  $this->hosters=$this->topHostersByKind($kind,$limit);
+  return $this->hosters;
 }
 }
