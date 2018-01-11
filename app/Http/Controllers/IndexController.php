a0061b8dde49927d2a25c53a6394d143907d4ce8
Vadim <vadim@hyperweb.com.ua>
2018-01-11 16:42:48 +0200

    added variables to link into query class for getting top hosters by kind:vps,vds,vps serer

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,14 +11,17 @@ class IndexController extends Controller
   private $query;
 
   public function index(){
-
    $query = new IndexQueries();
-     //get top hosters
-   $top_hosters_summary =  $query->getTopHosters();
-   //gte top hosters by region
-  $top_hosters_by_region_ru = $query->getTopHostersByRegion('RU');
-  $top_hosters_by_region_uk = $query->getTopHostersByRegion('UK');
+    //get top hosters
+   $top_hosters_summary            = $query->getTopHosters();
+    //gte top hosters by region
+   $top_hosters_by_region_ru       = $query->getTopHostersByRegion('RU');
+   $top_hosters_by_region_uk       = $query->getTopHostersByRegion('UK');
+    //get top hosters by kind (vps, vds)
+   $top_hosters_by_kind_vps        = $query->getTopHostersByKind('vps',10);
+   $top_hosters_by_kind_vds        = $query->getTopHostersByKind('vds',5);
+   $top_hosters_by_kind_vps_server = $query->getTopHostersByKind('vps server',5);
 
-   return view('index',compact('top_hosters_summary', 'top_hosters_by_region_ru', 'top_hosters_by_region_uk'));
+   return view('index',compact('top_hosters_summary', 'top_hosters_by_region_ru', 'top_hosters_by_region_uk','top_hosters_by_kind_vps','top_hosters_by_kind_vds','top_hosters_by_kind_vps_server'));
  }
 }
