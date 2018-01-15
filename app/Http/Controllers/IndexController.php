1ae5131fdef9e7ce03cea29bef500a2f8c0110e0
Vadim <vadim@hyperweb.com.ua>
2018-01-15 14:15:31 +0200

    adding all methods for working with left side cpolumn (top of hosters by kind and locations)

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\IndexQueries;
+// include_once('./../../Libs/url_slug.php');
+// include_once('app_path()/Libs/test.php');
 
 class IndexController extends Controller
 {
@@ -18,10 +20,9 @@ class IndexController extends Controller
    $top_hosters_by_region_ru       = $query->getTopHostersByRegion('RU');
    $top_hosters_by_region_uk       = $query->getTopHostersByRegion('UK');
     //get top hosters by kind (vps, vds)
-   $top_hosters_by_kind_vps        = $query->getTopHostersByKind('vps',10);
-   $top_hosters_by_kind_vds        = $query->getTopHostersByKind('vds',5);
+   $top_hosters_by_kind_vps        = $query->getTopHostersByKind('shared',10);
+   $top_hosters_by_kind_vds        = $query->getTopHostersByKind('vds server',5);
    $top_hosters_by_kind_vps_server = $query->getTopHostersByKind('vps server',5);
-
    return view('index',compact('top_hosters_summary', 'top_hosters_by_region_ru', 'top_hosters_by_region_uk','top_hosters_by_kind_vps','top_hosters_by_kind_vds','top_hosters_by_kind_vps_server'));
  }
 }
