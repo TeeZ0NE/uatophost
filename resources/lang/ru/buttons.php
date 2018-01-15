e6bbe1c8ed6c3657edf28ff585c2d6d1289ec799
Vadim <vadim@hyperweb.com.ua>
2018-01-15 14:12:54 +0200

    changing translates to new mode

--- resources/lang/ru/buttons.php
+++ resources/lang/ru/buttons.php
@@ -1,11 +1,12 @@
 <?php
 return [
-  "defaultCaptionNext"=>"Далее",
-  "goToCompare" =>"Сравнить",
-  "gotoHostingRate"=>"Перейти к рейтингу",
-  "readAllText" => "Читать весь текст",
-  "readMore" => "Подробнее",
-  "readOurBlog"=>"Читать блог",
-  "seeAllPromotions" => "Смотреть все акции",
+  "next"=>"Далее",
+  "compare" =>"Сравнить",
+  "goto_hosting_rate"=>"Перейти к рейтингу",
+  "read_all" => "Читать весь текст",
+  "read_more" => "Подробнее",
+  "read_our_blog"=>"Читать блог",
+  "see_all_promotions" => "Смотреть все акции",
   "subscribe"=>"Подписаться",
+  "hint_goto_hosting_rate"=>"На страницу с рейтингом",
 ];
